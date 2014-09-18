ea3848ced444854a1d210ee4ee304790c2170606
Juliana Lucena <julianalucenaa@gmail.com>
2014-09-18 19:04:10 -0300

    Configures SendGrid at production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 587,
+    domain:               'tedxrecife.com.br',
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
