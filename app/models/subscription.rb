507491826fedd99aa236c2d57a77bfdd9ed1b84f
Juliana Lucena <julianalucenaa@gmail.com>
2014-09-19 14:07:51 -0300

    Turns social-networks at Subscription optional.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,7 +2,7 @@ require 'valid_email'
 
 class Subscription < ActiveRecord::Base
   validates :name, :email, :cpf, :birthday, :corporation, :message,
-    :social_networks, presence: true
+    presence: true
   validates :email, email: true, if: :email_changed?
   validates :cpf, cpf: true, if: :cpf_changed?
 end
