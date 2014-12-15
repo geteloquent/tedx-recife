d3dbb66744325423cc660bd58404d21faee1c98f
Juliana Lucena <julianalucenaa@gmail.com>
2014-12-15 09:25:01 -0300

    Adds talks page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,11 @@ module ApplicationHelper
     file = YAML.load_file('config/speakers.yml')
     @speakers = file.fetch('speakers')
   end
+
+  def talks
+    return @talks if @talks
+
+    file = YAML.load_file('config/talks.yml')
+    @talks = file.fetch('talks')
+  end
 end
